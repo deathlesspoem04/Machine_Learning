3ca04ad49d6e0ce3d11f8ac21197b25bc9b67fb2
deathlesspoem04 <sinha.vishesh2000@gmail.com>
2020-03-02 00:21:54 +0530

    kmeans.py

--- K-Means_Clustering/kmeans.py
+++ K-Means_Clustering/kmeans.py
@@ -27,11 +27,11 @@ kmeans = KMeans(n_clusters=5, init='k-means++', max_iter=300, n_init=10, random_
 y_kmeans = kmeans.fit_predict(X)
 
 # Visualising the Clusters
-plt.scatter(X[y_kmeans== 0,0],X[y_kmeans == 0,1], s=100, c='red', label='Cluster1')
-plt.scatter(X[y_kmeans== 1,0],X[y_kmeans == 1,1], s=100, c='blue', label='Cluster2')
-plt.scatter(X[y_kmeans== 2,0],X[y_kmeans == 2,1], s=100, c='orange', label='Cluster3')
-plt.scatter(X[y_kmeans== 3,0],X[y_kmeans == 3,1], s=100, c='Yellow', label='Cluster4')
-plt.scatter(X[y_kmeans== 4,0],X[y_kmeans == 4,1], s=100, c='green', label='Cluster5')
+plt.scatter(X[y_kmeans== 0,0],X[y_kmeans == 0,1], s=100, c='red', label='Cluster1/Careful')
+plt.scatter(X[y_kmeans== 1,0],X[y_kmeans == 1,1], s=100, c='blue', label='Cluster2/Standard')
+plt.scatter(X[y_kmeans== 2,0],X[y_kmeans == 2,1], s=100, c='orange', label='Cluster3/Target')
+plt.scatter(X[y_kmeans== 3,0],X[y_kmeans == 3,1], s=100, c='Yellow', label='Cluster4/Careless')
+plt.scatter(X[y_kmeans== 4,0],X[y_kmeans == 4,1], s=100, c='green', label='Cluster5/Sensible')
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='cyan', label='Centeroids')
 plt.title('Clusters of customers')
 plt.xlabel('Annual Income (k$)')
