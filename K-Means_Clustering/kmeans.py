8330568be0eea6e15632d5a74398ac50c7315102
deathlesspoem04 <sinha.vishesh2000@gmail.com>
2020-03-02 22:18:53 +0530

    kmeans.py

--- K-Means_Clustering/kmeans.py
+++ K-Means_Clustering/kmeans.py
@@ -12,11 +12,11 @@ X = dataset.iloc[:, [3,4]].values
 # Using the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
 wcss = []
-for i in range(1,11):
+for i in range(1, 11):
     kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
-plt.plot(range(1,11), wcss)
+plt.plot(range(1, 11), wcss)
 plt.title('The Elbow Method')
 plt.xlabel('No. of Clusters')
 plt.ylabel('WCSS')
